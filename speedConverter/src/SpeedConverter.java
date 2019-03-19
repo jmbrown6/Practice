efc6102b67053c5c57c211d681a22a402ca79d0c
unknown <jmbrown6@aggies.ncat.edu>
2019-03-19 01:32:12 -0400

    speed converter class added

--- speedConverter/src/SpeedConverter.java
+++ speedConverter/src/SpeedConverter.java
@@ -2,7 +2,10 @@ public class SpeedConverter {
     public static double toMilesPerHour (double kilometersPerHour){
         if(kilometersPerHour < 0){
             return -1;
-        }else if(kilometersPerHour > 0){}
+        }else if(kilometersPerHour > 0){
+
+            System.out.println("Hello world");
+        }
 
         return kilometersPerHour;
 

71daa9b2ded3a2dbff9dc24e093d3e1518691763
unknown <jmbrown6@aggies.ncat.edu>
2019-03-19 01:23:04 -0400

    initial commit

---
+++ speedConverter/src/SpeedConverter.java
@@ -0,0 +1,10 @@
+public class SpeedConverter {
+    public static double toMilesPerHour (double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            return -1;
+        }else if(kilometersPerHour > 0){}
+
+        return kilometersPerHour;
+
+    }
+}
